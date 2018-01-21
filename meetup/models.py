95ab2b00ec260f57a4ecc67518fc0aacbfd240ce
kallepallimounikasmitha <k.mounikasmitha999@gmail.com>
2018-01-21 12:01:35 +0530

    Written models

--- meetup/models.py
+++ meetup/models.py
@@ -4,3 +4,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Meetups(models.Model):
+    Description = models.CharField(max_length=500)
+    Date  = models.DateField()
+    Time =  models.TimeField()
+    Venue = models.CharField(max_length=50)
+    Topic = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.Description
+
+
+
+
